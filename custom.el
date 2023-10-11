b4466218ae21cd23f025b76d4748de3ca5cc2353
Sinthoras39 <digc0820@gmail.com>
2023-10-11 16:09:10 +0200

    linux init

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(flycheck-python-pycompile-executable "python")
+ '(flycheck-python-pylint-executable "python")
+ '(flycheck-python-pyright-executable "python")
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(lsp-pyright all-the-icons))
+ '(python-shell-exec-path '("python"))
+ '(python-shell-interpreter "python")
+ '(transient-mark-mode t)
+ '(treemacs-python-executable "python"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
